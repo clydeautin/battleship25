3916dbcac3f0b887d00ce1425e77a6fffd79d851
Clyde Autin <clydeautin@Clydes-MacBook-Pro-2.local>
2025-12-22 15:56:45 -0800

    valid placement on board is implemented

--- battleship25/board.py
+++ battleship25/board.py
@@ -14,3 +14,50 @@ class Board:
             for col in cols:
                 coord = f"{row}{col}"
                 self.cells[coord] = Cell(coord)
+
+    def is_valid_coordinate(self, coord: str) -> bool:
+        return coord in self.cells
+    
+    def is_valid_placement(self, ship: object, placement: list) -> bool:
+        ship_n = ship.length
+        placement_n = len(placement)
+        if ship_n != placement_n:
+            return False
+        #checks if every coordinate in the placement array is valid on game board
+        for x in placement:
+            if self.is_valid_coordinate(x) is False:
+                return False
+        
+        # need to determine if ship is place horizontally or vertically
+        row_nums = []
+        col_nums = []
+
+        for x in placement:
+            row_nums.append(ord(x[0]))
+            col_nums.append(int(x[1:]))
+
+        #determine orientation by converting row nums into a set to remove duplicates so that if all rows are the same we only get one value
+        same_row = len(set(row_nums)) == 1
+        same_col = len(set(col_nums)) == 1
+
+        # horizontal: same row, columns consecutive
+        if same_row and self._consecutive(col_nums):
+            return True
+        
+        # vertical: same column, rows consecutive
+        if same_col and self._consecutive(row_nums):
+            return True
+        
+        return False
+
+    def _consecutive(self, nums: list[int]) -> bool:
+        nums = sorted(nums)
+
+        for i in range(len(nums) - 1):
+            current = nums[i]
+            next_val = nums[i + 1]
+
+            if next_val - current != 1:
+                return False
+        
+        return True
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,6 @@
 import pytest
 from battleship25.cell import Cell
+from battleship25.ship import Ship
 from battleship25.board import Board
 
 def test_board_has_cells():
@@ -9,3 +10,26 @@ def test_board_has_cells():
     assert "A1" in board.cells
     assert isinstance(board.cells["A1"], Cell)
 
+def test_validating_coordinates():
+    board = Board()
+    assert board.is_valid_coordinate("A1") == True
+    assert board.is_valid_coordinate("B4") == True
+    assert board.is_valid_coordinate("Z9") == False
+    assert board.is_valid_coordinate("C22") == False
+
+def test_valid_placement():
+    board = Board()
+    cruiser = Ship("Cruiser", 3)
+    submarine = Ship("Submarine", 2)
+    # number of coordinates in the array should be the same as the length of the ship:
+    assert board.is_valid_placement(cruiser, ["A1", "A2"]) == False
+    assert board.is_valid_placement(submarine, ["A1", "A2", "A3"]) == False
+    # check coordinates are consecutive
+    assert board.is_valid_placement(cruiser, ["A1", "A2", "A4"]) == False
+    assert board.is_valid_placement(submarine, ["A1", "C1"]) == False
+    # coordinates are not diagonal
+    assert board.is_valid_placement(cruiser, ["A1", "B2", "C3"]) == False
+    assert board.is_valid_placement(submarine, ["C2", "D3"]) == False
+    # If all the previous checks pass then the placement should be valid:
+    assert board.is_valid_placement(cruiser, ["A1", "A2", "A3"]) == True
+    assert board.is_valid_placement(submarine, ["C3", "D3"]) == True
