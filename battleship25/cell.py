1d219ed8fd1b6544000b136abc737f2a915f1698
Clyde Autin <clydeautin@Clydes-MacBook-Pro-2.local>
2025-12-22 11:18:00 -0800

    cell and logic now full implemented to render

--- battleship25/cell.py
+++ battleship25/cell.py
@@ -3,7 +3,22 @@ class Cell:
         self.coordinate = coordinate
         self.ship = None
         self.empty = True
+        self.is_fired_upon = False
     
     def place_ship(self, ship: object):
         self.ship = ship
         self.empty = False
+
+    def fire_upon(self):
+        self.is_fired_upon = True
+        if self.ship is not None:
+            self.ship.hit() 
+
+    def render(self, reveal=False) -> str:
+        if not self.is_fired_upon:
+            if self.ship and reveal:
+                return "S"
+            return "."
+        if self.ship:
+            return "X" if self.ship.sunk() else "H"
+        return "M"   
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -15,3 +15,30 @@ def test_cell_can_hold_ship():
     assert cell.ship == cruiser
     assert cell.empty == False
 
+def test_cell_can_tell_its_fired_upon():
+    cell = Cell("B4")
+    cruiser = Ship("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert cell.is_fired_upon == False
+    cell.fire_upon()
+    assert cell.ship.health == 2
+    assert cell.is_fired_upon == True
+
+def test_cell_render_state():
+    cell_1 = Cell("B4")
+    assert cell_1.render() == "."
+    cell_1.fire_upon()
+    assert cell_1.render() == "M"
+
+def test_cell_render_with_ship():
+    cell_2 = Cell("C3")
+    cruiser = Ship("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    assert cell_2.render() == "."
+    assert cell_2.render(True) == "S"
+    cell_2.fire_upon()
+    assert cell_2.render() == "H"
+    cruiser.hit()
+    cruiser.hit()
+    assert cruiser.sunk() == True
+    assert cell_2.render() == "X"
